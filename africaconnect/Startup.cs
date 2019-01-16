d15e6262a316f8800acf0fae8f9d107b919adabf
kzish <zishumbak@gmail.com>
2019-01-16 17:30:32 +0100

    hassan

--- africaconnect/Startup.cs
+++ africaconnect/Startup.cs
@@ -35,23 +35,14 @@ namespace africaconnect
             services.AddSession();
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>(); // <= Add this for pagination
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-           /* services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.KnownProxies.Add(IPAddress.Parse("54.93.36.66"));
-            });*/
+          
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            
+
 
             app.UseHttpsRedirection();
             app.UseCookiePolicy();
@@ -60,10 +51,10 @@ namespace africaconnect
             app.UseMvc();
 
            
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            /*app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
                 ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+            });*/
 
             app.UseAuthentication();
 
